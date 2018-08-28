67e292ab99ec6b8dbc72f8d0ce3564bb7c1ddeda
Mian Saleem <saleem@tecdiary.com>
2018-08-28 20:33:59 +0800

    Add docs folder

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -13,7 +13,7 @@ module.exports = {
         ["meta", { name: "msapplication-TileImage", content: "/icons/msapplication-icon-144x144.png" }],
         ["meta", { name: "msapplication-TileColor", content: "#000000" }]
     ],
-    dest: "dist",
+    dest: "docs",
     serviceWorker: true,
     themeConfig: {
         repo: "tecdiary/sbm-guide",
